92684095fee8a932470d86c71304741896f4a157
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-11-30 12:19:09 +0500

    включил логирование sql запросов

--- prisma/client.ts
+++ prisma/client.ts
@@ -2,7 +2,34 @@
 import { PrismaClient } from '../../../../../../../app/generated/prisma-client'
 
 const prismaClientSingleton = () => {
-    return new PrismaClient();
+    const prisma = new PrismaClient({
+        log: [
+            {
+                emit: 'event',
+                level: 'query',
+            },
+            {
+                emit: 'stdout',
+                level: 'error',
+            },
+            {
+                emit: 'stdout',
+                level: 'info',
+            },
+            {
+                emit: 'stdout',
+                level: 'warn',
+            },
+        ],
+    })
+
+    prisma.$on('query', (e) => {
+        console.log('Query: ' + e.query)
+        console.log('Params: ' + e.params)
+        console.log('Duration: ' + e.duration + 'ms')
+    })
+
+    return prisma
 };
 
 declare global {
