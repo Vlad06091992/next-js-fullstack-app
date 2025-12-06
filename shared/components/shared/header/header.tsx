027da3cc04586a080e775faa890c41a1f43673df
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-12-06 16:37:59 +0500

    вернул строку поиска продуктов

--- shared/components/shared/header/header.tsx
+++ shared/components/shared/header/header.tsx
@@ -1,9 +1,10 @@
 import {cn} from "@/shared/lib/utils";
-import {Container} from "@/shared/components";
+import {Container, SearchInput} from "@/shared/components";
 import Image from "next/image";
 import Logo from '@/public/logo.png'
 import {Button} from "@/shared/components/ui";
-import {ArrowRight, ShoppingCart, User} from "lucide-react";
+import {ArrowRight, Search, ShoppingCart, User} from "lucide-react";
+import Link from "next/link";
 
 
 interface HeaderProps {
@@ -15,12 +16,18 @@ export const Header = ({className}: HeaderProps) => {
     return (<header className={cn('', className)}>
         <Container className='flex items-center justify-between py-4'>
             {/* левая часть   */}
-            <div className={"flex gap-4 items-center"}>
-                <Image src={Logo} alt={'logo'} width={32} height={32}/>
-                <div>
-                    <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
-                    <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+            <Link href={'/'}>
+                <div className={"flex gap-4 items-center"}>
+                    <Image src={Logo} alt={'logo'} width={32} height={32}/>
+                    <div>
+                        <h1 className="text-2xl uppercase font-black">Next Pizza</h1>
+                        <p className="text-sm text-gray-400 leading-3">вкусней уже некуда</p>
+                    </div>
                 </div>
+            </Link>
+
+            <div className={'mx-10 flex-1'}>
+                <SearchInput/>
             </div>
 
             <div className={"flex gap-4 items-center"}>
