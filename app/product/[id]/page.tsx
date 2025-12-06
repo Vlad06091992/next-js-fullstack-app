621245a0602c940a76641e228dec885a37e84d4a
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-12-06 19:10:30 +0500

    базовая реализация страницы с продуктом

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,12 +1,45 @@
-import {Categories, Container, Filters, SortPopup, Title} from "@/shared/components";
-import {TopBar} from "@/shared/components/shared/top-bar/top-bar";
-import {ProductCard} from "@/shared/components/shared/product-card/product-card";
-import {ProductsGroupList} from "@/shared/components/shared/products-group-list/products-group-list";
+import {Container, ProductImage, Title} from "@/shared/components";
+import {prisma} from "@/prisma/client";
+import {notFound} from "next/navigation";
+import {GroupVariants} from "@/shared/components/shared/group-variants/group-variants";
 
-export default function Product({params:{id}}:{params:{id: string}}) {
+
+export default async function Product({params: {id}}: { params: { id: string } }) {
+
+    const product = await prisma.product.findFirst({where: {id}});
+
+
+    if (!product) {
+        return notFound();
+    }
+
     return (
-        <div>
-            Продукт: {id}
-        </div>
+        <Container className={"flex flex-col my-10"}>
+            <div className="flex flex-1">
+                <ProductImage size={40} name={product.name} imageUrl={product.imageUrl}/>
+            <div className={"w-[490px] bg-[#f7f6f5] p-7"}>
+                <Title text={product.name} size="md" className="font-extrabold mb-1" />
+                <p className="text-gray-400">{"fksdfjsdlk"}</p>
+                <GroupVariants value={"2"}  items={[
+                    {
+                        name: "Маленькая",
+                        value: "1",
+                        disabled:true
+                    },
+                    {
+                        name: "Средняя",
+                        value: "2",
+                        disabled:false,
+                    },
+                    {
+                        name: "Большая",
+                        value: "3",
+                        disabled:false
+                    },
+                ]}
+                />
+            </div>
+            </div>
+        </Container>
     );
 }
