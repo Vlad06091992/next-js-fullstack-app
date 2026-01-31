68da103af6e88077b8a498263d6ef549f79f6756
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2026-01-31 09:37:50 +0500

    функционал отправки письма

--- shared/lib/index.ts
+++ shared/lib/index.ts
@@ -3,6 +3,7 @@ export {getAvailablePizzaSizes} from './get-availlable-pizza-sizes'
 export {getPizzaDetails} from './get-pizza-details'
 export {getCartItemDetails} from './get-cart-item-details'
 export {getCartDetails} from './get-cart-details'
+export {sendEmail} from './send-email'
 
 //TODO добавить импорты
 //почему реэкспорт функций, использующих призму ломает приложение?????
